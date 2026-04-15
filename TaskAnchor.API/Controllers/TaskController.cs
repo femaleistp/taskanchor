74f93178c9f5ca9c73d41814941c609c1938f1d8
Brittany Hancock <femaleentp@gmail.com>
2026-04-15 14:00:15 -0700

    Fix TasksController discovery and align task key usage

---
+++ TaskAnchor.API/Controllers/TaskController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskAnchor.API.Data;
+using TaskAnchor.API.Models;
+using TaskAnchor.API.Services;
+using TaskStatus = TaskAnchor.API.Models.TaskStatus;
+
+namespace TaskAnchor.API.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    public class TasksController : ControllerBase
+    {
+        // Implement your API endpoints here
+        private readonly TaskAnchorDbContext _context;
+
+        public TasksController(TaskAnchorDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask([FromBody] TaskItem task)
+        {
+            // Logic to create a new task
+            task.Status = TaskStatus.Active;
+            task.LastUpdatedDate = TaskTimestampRules.GetUpdatedTimestamp();
+
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(CreateTask), new { id = task.TaskId }, task);
+        }
+    }
+}
--- TaskAnchor.API/Program.cs
+++ TaskAnchor.API/Program.cs
@@ -3,15 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<TaskAnchorDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
