df69f872f3e1096b01973e7c638dc73c9085d3b7
Brittany Hancock <femaleentp@gmail.com>
2026-04-15 13:15:06 -0700

    Register TaskAnchorDbContext in Program.cs

--- TaskAnchor.API/Program.cs
+++ TaskAnchor.API/Program.cs
@@ -1,4 +1,10 @@
+using TaskAnchor.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TaskAnchorDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
