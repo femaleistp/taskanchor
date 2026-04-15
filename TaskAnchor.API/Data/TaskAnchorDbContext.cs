77ee4635f51be1ed95d4f742657ad28a34d0da39
Brittany Hancock <femaleentp@gmail.com>
2026-04-15 13:13:35 -0700

    Add TaskAnchorDbContext in Data folder

---
+++ TaskAnchor.API/Data/TaskAnchorDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskAnchor.API.Models;
+
+namespace TaskAnchor.API.Data
+{
+    public class TaskAnchorDbContext : DbContext
+    {
+        public TaskAnchorDbContext(DbContextOptions<TaskAnchorDbContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<TaskItem> Tasks { get; set; }
+        public DbSet<ProgressLogEntry> ProgressLogEntries { get; set; }
+    }
+}
